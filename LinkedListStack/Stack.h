7c561b307ad138cec27f464ea9fb83cee1e85d6a
Lokesh <lokeypa@hotmail.com>
2019-04-24 13:24:24 +0530

    Added defination for psuh and creared head for the  linklist

--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -2,9 +2,27 @@
 
 class Stack
 {
+private: 
+	class Element {
+	public: 
+		Element(int val, Element* pNext) :
+			val(val),
+			pNext(pNext)
+		{}
+	private:
+		int val;
+		Element* pNext = nullptr;
+
+	};
+
 public:
-	void Push( int val );
+	void Push(int val) {
+		pTop = new Element(val, pTop);
+	}
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+
+private :
+	Element* pTop = nullptr;	
 };
