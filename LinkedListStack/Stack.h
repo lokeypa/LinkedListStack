2324dcb43c42c7c5ca4e533b8060e6f45f929361
akashs <akashs@chetu.com>
2019-04-24 20:46:23 +0530

    Done logics with failing test 6

--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -9,6 +9,30 @@ private:
 			val(val),
 			pNext(pNext)
 		{}
+
+		int GetVal() const {
+			return val;
+		}
+
+		Element* Disconnect() {
+			auto pTemp = pNext;
+			pNext = nullptr;
+			return pTemp;
+		}
+
+		int CountElement() const {
+
+			// recurssion using 
+			if (pNext != nullptr) {
+				return pNext->CountElement() + 1;
+			}
+			else
+			{
+				return 1;
+			}
+
+		}
+
 	private:
 		int val;
 		Element* pNext = nullptr;
@@ -19,9 +43,32 @@ public:
 	void Push(int val) {
 		pTop = new Element(val, pTop);
 	}
-	int Pop();
-	int Size() const;
-	bool Empty() const;
+	int Pop() {
+
+		if (!Empty()) {
+			const int tempVal = pTop->GetVal();
+			auto pOldTop = pTop;
+			pTop = pTop->Disconnect();
+			delete pOldTop;
+			return tempVal;
+		}
+		else {
+			return -1;//if the stack ios empty.
+		}
+	}
+	int Size() const
+	{
+		if (!Empty()) {
+			return (*pTop).CountElement();
+		}
+		else {
+			return 0;
+		}
+	}
+	bool Empty() const
+	{
+		return pTop == nullptr;
+	}
 
 private :
 	Element* pTop = nullptr;	
