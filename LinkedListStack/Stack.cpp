27a0b047942b7cc4b3dfa6a4854a1acd29d4e28d
Lokesh <lokeypa@hotmail.com>
2019-05-16 13:00:03 +0530

    Created some metods temporary

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,20 @@
 #include "Stack.h"
+
+void Stack::Push(int val)
+{
+}
+
+int Stack::Pop()
+{
+	return 0;
+}
+
+int Stack::Size() const
+{
+	return 0;
+}
+
+bool Stack::Empty() const
+{
+	return false;
+}
