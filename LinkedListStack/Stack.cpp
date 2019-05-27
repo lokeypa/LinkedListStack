791e96ec4b5123e79c8145e70df16c618433649c
Lokesh <lokeypa@hotmail.com>
2019-05-27 21:39:48 +0530

    Added default and copy constructor and destructor to remove memory leaks.

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,5 +1,26 @@
 #include "Stack.h"
 
+Stack::Stack(const Stack & src)
+{ 
+	*this = src;
+}
+
+Stack & Stack::operator=(const Stack & src)
+{
+	// TODO: insert return statement here
+	if (!src.Empty())
+	{
+		pTop = new Element(*src.pTop);
+	}
+	return *this;
+}
+
+Stack::~Stack()
+{
+	delete pTop;
+	pTop = nullptr;
+}
+
 void Stack::Push(int val)
 {
 	pTop = new Element(val, pTop);
@@ -61,5 +82,20 @@ int Stack::Element::CountElement()
 	}
 	else {
 		return 1 + pNext->CountElement();
+	}
+}
+
+Stack::Element::~Element()
+{
+	delete pNext;
+	pNext = nullptr;
+}
+
+Stack::Element::Element(const Element & src)
+	:
+	value(src.value)
+{
+	if (src.pNext != nullptr) {
+		pNext = new Element(*src.pNext);
 	}
 }
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -10,17 +10,20 @@ private:
 	class Element {
 	public:
 		Element(int value, Element *pNext);
+		Element(const Element& src);
 		int GetValue();
 		Element* Disconnect();
 		int CountElement();
+		~Element();
 	private:
 		int value;
 		Element *pNext = nullptr;
 	};
 public:
+	Stack() = default;
 	Stack(const Stack& src);
 	Stack& operator=(const Stack& src); // need to do deep copy.
-
+	~Stack();
 	void Push( int val );
 	int Pop();
 	int Size() const;
